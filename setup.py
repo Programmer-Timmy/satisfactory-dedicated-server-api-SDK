23151347c7d22839828a131ed83464fef4b11786
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-10-01 23:35:24 +0200

    remove tests and exmapels form installing

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ readme_path = os.path.join(root_dir, 'README.md')
 
 setup(
     name='satisfactory_api_client',
-    version='0.1.3',
-    packages=find_packages(),
+    version='0.1.4',
+    packages=find_packages(exclude=['tests', 'examples']),
     install_requires=[
         "python-dotenv~=1.0.1",
         "urllib3~=2.2.3",
