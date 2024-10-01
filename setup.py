f5eaa3c87a21bc0410e93f74b76f815022741510
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2024-10-01 22:56:38 +0200

    update read me and more

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='satisfactory_api_client',
     version='0.1.2',
     packages=find_packages(),
-    install_requires=[req.strip() for req in open('requirements.txt').readlines()],
+    install_requires=[req.strip() for req in open('/requirements.txt').readlines()],
     description='A Python Package for interacting with the Satisfactory Dedicated Server API',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
